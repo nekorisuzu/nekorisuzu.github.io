8c592c24a3bc8b2d10f161dc5baaf26f152eb577
nekorisuzu <htmlen@proton.me>
2026-07-02 03:30:03 +0800

    Create background.js

---
+++ background.js
@@ -0,0 +1,85 @@
+let branches = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  background(255);
+  stroke(0);
+  strokeWeight(1);
+
+  for (let i = 0; i < 40; i++) {
+    spawn(random(width), random(height));
+  }
+}
+
+function draw() {
+
+
+  noStroke();
+  fill(255, 12);
+  rect(0, 0, width, height);
+
+  stroke(0);
+
+  for (let i = branches.length - 1; i >= 0; i--) {
+
+    let b = branches[i];
+
+    let x2 = b.x + cos(b.dir) * b.step;
+    let y2 = b.y + sin(b.dir) * b.step;
+
+    line(b.x, b.y, x2, y2);
+
+    b.x = x2;
+    b.y = y2;
+    b.life--;
+
+    if (random() < 0.35) {
+      b.dir += random([
+        HALF_PI,
+        -HALF_PI,
+        PI/4,
+        -PI/4,
+        PI,
+        0
+      ]);
+    }
+
+    if (random() < 0.02 && branches.length < 120) {
+      branches.push({
+        x: b.x,
+        y: b.y,
+        dir: b.dir + random(-PI/2, PI/2),
+        step: random(6, 16),
+        life: random(20, 80)
+      });
+    }
+
+    if (
+      b.life < 0 ||
+      b.x < -20 || b.x > width + 20 ||
+      b.y < -20 || b.y > height + 20
+    ) {
+      branches.splice(i, 1);
+    }
+  }
+
+  while (branches.length < 40) {
+    spawn(random(width), random(height));
+  }
+}
+
+function spawn(x, y) {
+  branches.push({
+    x: x,
+    y: y,
+    dir: random(TWO_PI),
+    step: random(6, 16),
+    life: random(30, 100)
+  });
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(255);
+}
